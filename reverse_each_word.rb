2774bbd45d699efce1b57c0d2022059b537a1f96
Gee-Wey Yue <geewey@gmail.com>
2019-09-22 14:04:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,20 @@
-def reverse_each_word(str)
-  reversed_words = []
-  words = str.split(" ") # array
-  words.each do |word|
-    reversed_words << word.reverse
-  end
+# def reverse_each_word(str)
+#   reversed_words = []
+#   words = str.split(" ") # array
+#   words.each do |word|
+#     reversed_words << word.reverse
+#   end
   
-  return reversed_words.join(" ")
+#   return reversed_words.join(" ")
+# end
+
+
+def reverse_each_word(str)
+  words = str.split(" ")
+  
+  words.collect { |word| word.reverse }
+  
+  return words.join(" ")
 end
+
+
